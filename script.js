6eda9a5c78039a6695e0e9497fffaaed97584221
yuliia <pustovayajulia@gmail.com>
2023-05-04 19:39:07 +0300

    fix

--- script.js
+++ script.js
@@ -11,26 +11,21 @@
 //     console.log('OK!')
 // }
 
+let numOrStr = prompt('input number or string')
+switch (true) {
 
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
-
-switch (numOrStr) {
-    case null:
-        console.log("ви скасували");
-        break;
-    case '':
-        console.log("Empty String");
-        break;
-    default: {
-        if ((/^\d+$/.test(numOrStr)) || (/^[a-zA-Z]+$/.test(numOrStr))) {
-            console.log('OK');
-
-        } else {
-            console.log(' number is Ba_NaN')
-        }
-        break;
-    }
-
+  case numOrStr === null:
+    console.log('ви скасували')
+    break
+  case numOrStr.trim() === '':
+    console.log('Empty String')
+    break
+  case isNaN(+numOrStr):
+    console.log(' number is Ba_NaN')
+    break
+  default: {
+    console.log('OK')
+    break
+  }
 
 }
